ead17a7261f0eaf7883687ac3fec39ed66f05308
Romit Sovakar <121713992+punpun018@users.noreply.github.com>
2025-03-21 18:21:03 +0530

    Delete dfs.cpp

--- dfs.cpp
+++
@@ -1,9 +0,0 @@
-function<void(int)> dfs = [&](int cur)
-    {
-        vis[cur] = true;
-        for (auto it : adj[cur])
-        {
-            if (!vis[it])
-                dfs(it);
-        }
-    };
